525fa53b8bbea641c759208a559e4d0c4ade019a
brucemolinanyc <brucemolinanyc@gmail.com>
2020-01-28 16:41:14 -0500

    basic GUI framework created

--- mybooks.py
+++ mybooks.py
@@ -4,5 +4,9 @@ from tkinter import messagebox
 
 app  = Tk()
 
+app.title("My Books Database Application")
+app.configure(background="light blue")
+app.geometry("850x500")
+app.resizable(width=False, height=False)
 
 app.mainloop()
