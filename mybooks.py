61760f9d17f10fd1789148bafb1eae7882372c14
brucemolinanyc <brucemolinanyc@gmail.com>
2020-01-28 16:30:35 -0500

    added tkinter for a simple GUI/frontend

---
+++ mybooks.py
@@ -0,0 +1,8 @@
+from tkinter import Tk, Button, Label, Scrollbar, Listbox, StringVar, Entry, W, E, N, S, END
+from tkinter import ttk
+from tkinter import messagebox
+
+app  = Tk()
+
+
+app.mainloop()
