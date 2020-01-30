fda421719df41e651ed10ab452ba754e97d1307d
brucemolinanyc <brucemolinanyc@gmail.com>
2020-01-30 12:24:59 -0500

    basic GUI created for database connections

--- mybooks.py
+++ mybooks.py
@@ -6,7 +6,7 @@ app  = Tk()
 
 app.title("My Books Database Application")
 app.configure(background="light green")
-app.geometry("850x500")
+app.geometry("1000x500")
 app.resizable(width=False, height=False)
 
 title_label = ttk.Label(app, text="Title", background="light green", font=("TKDefaultFont", 16))
@@ -27,5 +27,31 @@ isbn_text = StringVar()
 isbn_entry = ttk.Entry(app, width=24, textvariable=isbn_text)
 isbn_entry.grid(row=0, column=5, sticky=W)
 
+add_btn = Button(app, text="Add Book", background="black", foreground="black", font="helvetica 10 bold", command="")
+add_btn.grid(row=0,column=6, sticky=W)
+
+list_box = Listbox(app, height=16, width=40, font="helvetica 13", bg="light blue")
+list_box.grid(row=3, column=1, columnspan=14, sticky=W + E, pady=40, padx=15)
+
+scroll_bar = Scrollbar(app)
+scroll_bar.grid(row=1, column=8, rowspan=14, sticky=W)
+
+list_box.configure(yscrollcommand=scroll_bar.set)
+scroll_bar.configure(command=list_box.yview)
+
+modify_btn = Button(app, text="Modify Record", background="black", foreground="black", font="helvetica 10 bold", command="")
+modify_btn.grid(row=15, column=4)
+
+delete_btn = Button(app, text="Delete Record", background="black", foreground="black", font="helvetica 10 bold", command="")
+delete_btn.grid(row=15, column=5)
+
+view_btn = Button(app, text="View All Records", background="black", foreground="black", font="helvetica 10 bold", command="")
+view_btn.grid(row=15, column=1)
+
+clear_btn = Button(app, text="Clear Screen", background="black", foreground="black", font="helvetica 10 bold", command="")
+clear_btn.grid(row=15, column=2)
+
+exit_btn = Button(app, text="Exit Application", background="black", foreground="black", font="helvetica 10 bold", command="")
+exit_btn.grid(row=15, column=3)
 
 app.mainloop()
