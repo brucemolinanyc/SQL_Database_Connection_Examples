c50f99c1e48473c194a940cf319513830f079d7d
brucemolinanyc <brucemolinanyc@gmail.com>
2020-01-30 12:32:01 -0500

    some code comments added for clarity

--- mybooks.py
+++ mybooks.py
@@ -2,13 +2,15 @@ from tkinter import Tk, Button, Label, Scrollbar, Listbox, StringVar, Entry, W, 
 from tkinter import ttk
 from tkinter import messagebox
 
-app  = Tk()
+app  = Tk() # Creates application window
 
+# Application window GUI - set to specific size that cannot be altered
 app.title("My Books Database Application")
 app.configure(background="light green")
 app.geometry("1000x500")
 app.resizable(width=False, height=False)
 
+# Labels and entry widgets
 title_label = ttk.Label(app, text="Title", background="light green", font=("TKDefaultFont", 16))
 title_label.grid(row=0, column=0, sticky=W)
 title_text = StringVar()
@@ -27,9 +29,12 @@ isbn_text = StringVar()
 isbn_entry = ttk.Entry(app, width=24, textvariable=isbn_text)
 isbn_entry.grid(row=0, column=5, sticky=W)
 
+# Button to add books
 add_btn = Button(app, text="Add Book", background="black", foreground="black", font="helvetica 10 bold", command="")
 add_btn.grid(row=0,column=6, sticky=W)
 
+
+# The area that holds all records for viewing
 list_box = Listbox(app, height=16, width=40, font="helvetica 13", bg="light blue")
 list_box.grid(row=3, column=1, columnspan=14, sticky=W + E, pady=40, padx=15)
 
@@ -39,6 +44,7 @@ scroll_bar.grid(row=1, column=8, rowspan=14, sticky=W)
 list_box.configure(yscrollcommand=scroll_bar.set)
 scroll_bar.configure(command=list_box.yview)
 
+# Additional buttons for CRUD purposes
 modify_btn = Button(app, text="Modify Record", background="black", foreground="black", font="helvetica 10 bold", command="")
 modify_btn.grid(row=15, column=4)
 
