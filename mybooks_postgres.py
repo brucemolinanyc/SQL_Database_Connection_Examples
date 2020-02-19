84e070b2bb1e555024c3e6931df43ecfbc2ef9e5
brucemolinanyc <brucemolinanyc@gmail.com>
2020-02-19 13:31:53 -0500

    postgres connection to python established

--- mybooks_postgres.py
+++ mybooks_postgres.py
@@ -92,7 +92,7 @@ def update_records():
 def on_closing():
     dd = db
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        root.destroy()
+        app.destroy()
         del dd
 
 
@@ -105,53 +105,59 @@ app.geometry("1000x500")
 app.resizable(width=False, height=False)
 
 # Labels and entry widgets
-title_label = ttk.Label(app, text="Title", background="light green", font=("TKDefaultFont", 16))
+
+title_label =ttk.Label(app,text="Title",background="light green",font=("TkDefaultFont", 16))
 title_label.grid(row=0, column=0, sticky=W)
 title_text = StringVar()
-title_entry = ttk.Entry(app, width=24, textvariable=title_text)
+title_entry = ttk.Entry(app,width=24,textvariable=title_text)
 title_entry.grid(row=0, column=1, sticky=W)
 
-author_label = ttk.Label(app, text="Author", background="light green", font=("TKDefaultFont", 16))
+author_label =ttk.Label(app,text="Author",background="light green",font=("TkDefaultFont", 16))
 author_label.grid(row=0, column=2, sticky=W)
 author_text = StringVar()
-author_entry = ttk.Entry(app, width=24, textvariable=author_text)
+author_entry = ttk.Entry(app,width=24,textvariable=author_text)
 author_entry.grid(row=0, column=3, sticky=W)
 
-isbn_label = ttk.Label(app, text="ISBN", background="light green", font=("TKDefaultFont", 14))
+isbn_label =ttk.Label(app,text="ISBN",background="light green",font=("TkDefaultFont", 14))
 isbn_label.grid(row=0, column=4, sticky=W)
 isbn_text = StringVar()
-isbn_entry = ttk.Entry(app, width=24, textvariable=isbn_text)
+isbn_entry = ttk.Entry(app,width=24,textvariable=isbn_text)
 isbn_entry.grid(row=0, column=5, sticky=W)
 
-# Button to add books
-add_btn = Button(app, text="Add Book", background="black", foreground="black", font="helvetica 10 bold", command="")
-add_btn.grid(row=0,column=6, sticky=W)
+# Add a button to insert inputs into database
 
+add_btn = Button(app, text="Add Book",bg="black",fg="black",font="helvetica 10 bold",command=add_book)
+add_btn.grid(row=0, column=6, sticky=W)
 
-# The area that holds all records for viewing
-list_box = Listbox(app, height=16, width=40, font="helvetica 13", bg="light blue")
-list_box.grid(row=3, column=1, columnspan=14, sticky=W + E, pady=40, padx=15)
+# Add  a listbox  to display data from database
+list_bx = Listbox(app,height=16,width=40,font="helvetica 13",bg="light blue")
+list_bx.grid(row=3,column=1, columnspan=14,sticky=W + E,pady=40,padx=15)
+list_bx.bind('<<ListboxSelect>>',get_selected_row)
 
+# Add scrollbar to enable scrolling
 scroll_bar = Scrollbar(app)
-scroll_bar.grid(row=1, column=8, rowspan=14, sticky=W)
+scroll_bar.grid(row=1,column=8, rowspan=14,sticky=W )
 
-list_box.configure(yscrollcommand=scroll_bar.set)
-scroll_bar.configure(command=list_box.yview)
+list_bx.configure(yscrollcommand=scroll_bar.set) # Enables vetical scrolling
+scroll_bar.configure(command=list_bx.yview)
 
-# Additional button widgets for CRUD purposes
-modify_btn = Button(app, text="Modify Record", background="black", foreground="black", font="helvetica 10 bold", command="")
+# Add more Button Widgets
+
+modify_btn = Button(app, text="Modify Record",bg="black",fg="black",font="helvetica 10 bold",command=update_records)
 modify_btn.grid(row=15, column=4)
 
-delete_btn = Button(app, text="Delete Record", background="black", foreground="black", font="helvetica 10 bold", command="")
+delete_btn = Button(app, text="Delete Record",bg="black",fg="black",font="helvetica 10 bold",command=delete_records)
 delete_btn.grid(row=15, column=5)
 
-view_btn = Button(app, text="View All Records", background="black", foreground="black", font="helvetica 10 bold", command="")
-view_btn.grid(row=15, column=1)
+view_btn = Button(app, text="View all records",bg="black",fg="black",font="helvetica 10 bold",command=view_records)
+view_btn.grid(row=15, column=1)#, sticky=tk.N)
 
-clear_btn = Button(app, text="Clear Screen", background="black", foreground="black", font="helvetica 10 bold", command="")
-clear_btn.grid(row=15, column=2)
+clear_btn = Button(app, text="Clear Screen",bg="black",fg="black",font="helvetica 10 bold",command=clear_screen)
+clear_btn.grid(row=15, column=2)#, sticky=tk.W)
 
-exit_btn = Button(app, text="Exit Application", background="black", foreground="black", font="helvetica 10 bold", command="")
+exit_btn = Button(app, text="Exit  Application",bg="black",fg="black",font="helvetica 10 bold",command=app.destroy)
 exit_btn.grid(row=15, column=3)
 
-app.mainloop()
+
+app.mainloop()  # Runs the application until exit
+
